8fe3ae29ec65c6c3bfd16ea894a67e37a6465fea
Jason Reid <jason@jasonreid.com>
2015-02-26 14:12:23 -0500

    Change script

--- hotspots.sh
+++ hotspots.sh
@@ -3,16 +3,17 @@
 OPTIND=1
 PREFIX=code_all
 AFTER=2014-01-01
+BEFORE=`date -v+1d +'%Y-%m-%d'`
 REPODIR=.
 SPREAD=1
 
 function show_help() {
 cat << ENDHELP
-    -r source repository root
-    -x unique prefix for data files
-    -d date from
-    -D date to
-    -s hotspot spread factor
+    -r source repository root (.)
+    -x unique prefix for data files (code_all)
+    -d date from (2014-01-01)
+    -D date to (today)
+    -s hotspot spread factor (1)
 ENDHELP
 }
 
@@ -26,6 +27,8 @@ while getopts "hr:x:d:s:" opt; do
 		x) PREFIX=${OPTARG}_all
 			;;
 		d) AFTER=${OPTARG}
+			;;
+		D) BEFORE=${OPTARG}
 			;;
 		s) SPREAD=${OPTARG}
 			;;
@@ -42,7 +45,7 @@ WD=$PWD
 cd $REPODIR
 
 cloc . --by-file --csv --quiet --report-file=$WD/data/${PREFIX}_lines.csv --exclude-dir=dist,out,lib,gems,.idea
-git log --pretty=format:'[%h] %an %ad %s' --date=short --numstat --after=${AFTER} --full-history . > $WD/data/${PREFIX}_evo.log
+git log --pretty=format:'[%h] %an %ad %s' --date=short --numstat --after=${AFTER} --before=${BEFORE} --full-history . > $WD/data/${PREFIX}_evo.log
 
 cd $WD
 
